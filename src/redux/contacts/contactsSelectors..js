5641377be1d1e7359d545cc566ed0fb92455f0e0
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-08-22 23:36:17 +0300

    add

--- src/redux/contacts/contactsSelectors..js
+++ src/redux/contacts/contactsSelectors..js
@@ -1,12 +1,13 @@
 import { createSelector } from '@reduxjs/toolkit';
-
+// простые селекторы
 export const getContacts = state => state.contacts.items;
 export const getLoading = state => state.contacts.isLoading;
 export const getError = state => state.contacts.error;
 export const getFilter = state => state.contacts.filter;
-
+//мемоизация.
 export const getVisibleContacts = createSelector(
-  [getContacts, getFilter],
+  [getContacts, getFilter], //аргументы. ссылки от чего зависит
+  //ф-ция делает вычисления. получает результат вызова простых селекторов
   (contacts, filter) => {
     const normalizedFilter = filter.toLowerCase();
 
